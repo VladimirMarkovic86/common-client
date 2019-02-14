e6c569233450c900a25fbeeafdf0b55d4577f149
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-14 17:48:15 +0100

    Task 54: Chat works through websocket connection now, and implemented Range header support for audio/video streaming.

--- src/cljs/common_client/chat/controller.cljs
+++ src/cljs/common_client/chat/controller.cljs
@@ -1,13 +1,35 @@
 (ns common-client.chat.controller
   (:require [ajax-lib.core :refer [sjax get-response]]
             [common-middle.request-urls :as rurls]
+            [common-middle.ws-request-actions :as wsra]
+            [htmlcss-lib.core :refer [gen div audio]]
             [js-lib.core :as md]
             [clojure.string :as cstring]
-            [language-lib.core :refer [get-label]]))
+            [cljs.reader :as reader]
+            [websocket-lib.core :refer [websocket]]))
+
+(def logged-in-username-a
+     (atom nil))
+
+(def websocket-obj-a
+     (atom nil))
+
+(defn scroll-chat-to-bottom
+  "Scrolls chat content to bottom"
+  []
+  (let [chat-history-el (md/query-selector-on-element
+                          ".chat"
+                          ".chat-history")]
+    (aset
+      chat-history-el
+      "scrollTop"
+      (.-scrollTopMax
+        chat-history-el))
+   ))
 
 (defn get-chat-users
   "Get all users for chat"
-  [logged-in-username]
+  []
   (let [xhr (sjax
               {:url rurls/get-chat-users-url})
         response (get-response
@@ -17,8 +39,8 @@
                         (fn [acc
                              elem]
                           (let [username (:username elem)]
-                            (if-not (= logged-in-username
-                                         username)
+                            (if-not (= @logged-in-username-a
+                                       username)
                               (conj
                                 acc
                                 elem)
@@ -28,77 +50,270 @@
                         users)]
     users-reduced))
 
-(defn get-chat-history
-  "Get chat history for selected user and logged in user"
-  [logged-in-username]
+(defn get-selected-chat-contact
+  "Returns value of selected option in user select html element"
+  []
   (when-let [selected-username-el (md/query-selector-on-element
                                     ".chat"
                                     "#chat-users")]
-    (let [selected-username (.-innerHTML
-                              (aget
-                                (.-selectedOptions
-                                  selected-username-el)
-                                0))
-          messages (atom [])]
-      (when-not (= selected-username
-                   logged-in-username)
-        (let [xhr (sjax
-                    {:url rurls/get-chat-history-url
-                     :entity [logged-in-username
-                              selected-username]})
-              response (get-response
-                         xhr)]
-          (reset!
-            messages
-            (:data response))
-         ))
-      @messages))
- )
-
-(defn send-chat-message
-  "Send message to selected user"
-  [{load-chat-history :evt-fn
-    logged-in-username :evt-p}
-   element
-   event]
-  (when-let [selected-username-el (md/query-selector-on-element
-                                    ".chat"
-                                    "#chat-users")]
-    (let [selected-username (.-innerHTML
+    (let [selected-username (.-value
                               (aget
                                 (.-selectedOptions
                                   selected-username-el)
                                 0))]
-      (when (and (not= selected-username
-                       logged-in-username)
-                 (not= selected-username
-                       (get-label 33))
-             )
-        (let [text-message-el (md/query-selector-on-element
-                                ".chat"
-                                "#chat-message-input")
-              text-message (md/get-value
-                             text-message-el)]
-          (when-not (cstring/blank?
-                      text-message)
-            (let [xhr (sjax
-                        {:url rurls/send-chat-message-url
-                         :entity {:usernames [logged-in-username
-                                              selected-username]
-                                  :message {:username logged-in-username
-                                            :text text-message}}
-                         })
-                  response (get-response
-                             xhr)]
-              (load-chat-history
-                logged-in-username)
-              (aset
-                text-message-el
-                "value"
-                ""))
-           ))
+      selected-username))
+ )
+
+(defn send-audio-chunk-ws
+  "Sends audio chunk through websocket"
+  [audio-chunk]
+  (let [selected-username (get-selected-chat-contact)]
+    (when (and (not= selected-username
+                     @logged-in-username-a)
+               (not= selected-username
+                     "-1"))
+      (when-not (cstring/blank?
+                  audio-chunk)
+        (let [message {:action wsra/send-audio-chunk-action
+                       :receiver selected-username
+                       :sender @logged-in-username-a
+                       :audio-chunk audio-chunk}]
+          (.send
+            @websocket-obj-a
+            (str
+              message))
+         ))
+     ))
+  )
+
+(def media-recorder-a
+     (atom nil))
+
+(def recording-a
+     (atom false))
+
+(def audio-el-a
+     (atom
+       (gen
+         (audio))
+      ))
+
+(def audio-chunks-a
+     (atom []))
+
+(def current-index-a
+     (atom 0))
+
+(defn play-chunks-fn
+  "Play chunks"
+  []
+  (let [audio-chunk-el (get
+                         @audio-chunks-a
+                         @current-index-a)]
+    (when audio-chunk-el
+      (aset
+        @audio-el-a
+        "src"
+        audio-chunk-el)
+      (.play
+        @audio-el-a)
+      (swap!
+        current-index-a
+        inc))
+   ))
+
+(defn initialize-media-recorder
+  "Initialize media recorder"
+  []
+  (reset!
+    media-recorder-a
+    nil)
+  (reset!
+    recording-a
+    false)
+  (reset!
+    audio-el-a
+    (gen
+      (audio
+        ""
+        nil
+        {:onended {:evt-fn play-chunks-fn}}))
+   )
+  (reset!
+    audio-chunks-a
+    [])
+  (reset!
+    current-index-a
+    0)
+  (let [media-devices (.-mediaDevices
+                        js/navigator)
+        audio-document (js-obj
+                         "audio" true)
+        audio-promise (.getUserMedia
+                        media-devices
+                        audio-document)]
+    (.then
+      audio-promise
+      ((fn []
+         (fn [stream]
+           (reset!
+             media-recorder-a
+             (js/MediaRecorder.
+               stream))
+           (.addEventListener
+             @media-recorder-a
+             "dataavailable"
+             ((fn []
+                (fn [event]
+                  (let [file-reader (js/FileReader.)
+                        onload (aset
+                                 file-reader
+                                 "onloadend"
+                                 ((fn []
+                                    (fn [e]
+                                      (send-audio-chunk-ws
+                                        (.-result
+                                          (.-target
+                                            e))
+                                       ))
+                                   ))
+                                )]
+                    (.readAsDataURL
+                      file-reader
+                      (.-data
+                        event))
+                   ))
+               ))
+            ))
+        ))
+     ))
+ )
+
+(defn make-chunks-fn
+  "Make chunks"
+  []
+  (when @recording-a
+    (.stop
+      @media-recorder-a)
+    (.start
+      @media-recorder-a)
+    (js/setTimeout
+      #(make-chunks-fn)
+      1000))
+  (when-not @recording-a
+    (.stop
+      @media-recorder-a))
+ )
+
+(defn start-streaming
+  "Start streaming audio to selected username"
+  []
+  (reset!
+    recording-a
+    true)
+  (make-chunks-fn))
+
+(defn stop-streaming
+  "Stop streaming audio to selected user"
+  []
+  (reset!
+    recording-a
+    false))
+
+(defn generate-chat-history
+  "Generate html of chat history"
+  [chat-history]
+  (let [messages (:messages chat-history)
+        html-messages (atom [])]
+    (doseq [{username :username
+             text :text
+             sent-at :sent-at} messages]
+      (swap!
+        html-messages
+        conj
+        (div
+          (div
+            text
+            {:class (if (= username
+                           @logged-in-username-a)
+                      "my-message"
+                      "contacts-message")})
+          {:class (if (= username
+                         @logged-in-username-a)
+                    "my-message-container"
+                    "contacts-message-container")})
+       ))
+    (gen
+      (div
+        @html-messages))
+   ))
+
+(defn get-chat-history
+  "Get chat history for selected user and logged in user"
+  []
+  (let [selected-username (get-selected-chat-contact)]
+    (when (not= selected-username
+                @logged-in-username-a)
+      (let [message {:action wsra/get-chat-history-action
+                     :usernames [@logged-in-username-a
+                                 selected-username]}]
+        (.send
+          @websocket-obj-a
+          (str
+            message))
        ))
    ))
+
+(defn selected-contact-username
+  "On every contact username change read
+   chat history and initialize media recorder"
+  []
+  (get-chat-history)
+  #_(initialize-media-recorder))
+
+(defn send-chat-message-ws
+  "Send chat message to contact"
+  [evt-p
+   element
+   event]
+  (let [selected-username (get-selected-chat-contact)]
+    (when (and (not= selected-username
+                     @logged-in-username-a)
+               (not= selected-username
+                     "-1"))
+      (let [text-message-el (md/query-selector-on-element
+                              ".chat"
+                              "#chat-message-input")
+            text-message (md/get-value
+                           text-message-el)]
+        (when-not (cstring/blank?
+                    text-message)
+          (let [message {:action wsra/send-chat-message-action
+                         :usernames [@logged-in-username-a
+                                     selected-username]
+                         :message {:username @logged-in-username-a
+                                   :text text-message}}
+                html-message-el (gen
+                                  (div
+                                    (div
+                                      text-message
+                                      {:class "my-message"})
+                                    {:class "my-message-container"}))]
+            (md/append-element
+              ".chat-history"
+              html-message-el)
+            (scroll-chat-to-bottom)
+            (.send
+              @websocket-obj-a
+              (str
+                message))
+            (aset
+              text-message-el
+              "value"
+              ""))
+         ))
+     ))
+ )
 
 (defn send-chat-message-on-enter
   "Send chat message on enter"
@@ -108,9 +323,142 @@
   (when (= (.-keyCode
              event)
            13)
-    (send-chat-message
+    (send-chat-message-ws
       evt-p
       element
       event))
  )
+
+(defn close-connection-ws-fn
+  "Closes websocket connection with server"
+  []
+  (let [message {:action wsra/close-connection-action
+                 :username @logged-in-username-a}]
+    (.send
+      @websocket-obj-a
+      (str
+        message))
+   ))
+
+(defn close-connection-if-user-left-chat
+  "Close connection if user left chat page"
+  []
+  (if (md/query-selector-on-element
+        ".content"
+        ".chat")
+    (js/setTimeout
+      #(close-connection-if-user-left-chat)
+      1000)
+    (close-connection-ws-fn))
+ )
+
+(defn establish-chat-connection-ws-fn
+  "Onopen websocket event save reference to websocket object"
+  [event]
+  (let [websocket-obj (.-target
+                        event)]
+    (try
+      (reset!
+        websocket-obj-a
+        websocket-obj)
+      (let [message {:action wsra/establish-connection-action
+                     :username @logged-in-username-a}]
+        (.send
+          @websocket-obj-a
+          (str
+            message))
+        (close-connection-if-user-left-chat))
+      websocket-obj
+      (catch js/Error e
+        (.error
+          js/console
+          e))
+     ))
+ )
+
+(defn chat-ws-onmessage-fn
+  "Onmessage websocket event receive message"
+  [event]
+  (let [response (reader/read-string
+                   (.-data
+                     event))
+        action (:action response)]
+    (when (= action
+             wsra/receive-chat-message-action)
+      (let [{username :username
+             text :text} (:message response)]
+        (when (= username
+                 (get-selected-chat-contact))
+          (let [html-message-el (gen
+                                  (div
+                                    (div
+                                      text
+                                      {:class "contacts-message"})
+                                    {:class "contacts-message-container"}))]
+            (md/append-element
+              ".chat-history"
+              html-message-el)
+            (scroll-chat-to-bottom))
+         ))
+     )
+    (when (= action
+             wsra/receive-chat-history-action)
+      (let [chat-history (:message response)
+            chat-history-html (generate-chat-history
+                                chat-history)]
+        (md/remove-element-content
+          ".chat-history")
+        (md/append-element
+          ".chat-history"
+          chat-history-html)
+        (scroll-chat-to-bottom))
+     )
+    (when (= action
+             wsra/receive-audio-chunk-action)
+      (let [{sender-username :sender
+             audio-chunk :audio-chunk} response]
+        (swap!
+          audio-chunks-a
+          conj
+          audio-chunk)
+        (play-chunks-fn))
+     )
+    (when (= action
+             "incoming-call")
+      ;Implement answer functionality
+     ))
+ )
+
+(defn websocket-default-close
+  "Default close of websocket"
+  [event]
+  (let [response (reader/read-string
+                   (.-reason
+                     event))
+        action (:action response)]
+    (when (= action
+             wsra/rejected-action)
+      (let [status (:status response)
+            message (:message response)]
+        (.log
+          js/console
+          (str
+            "status: "
+            status))
+        (.log
+          js/console
+          (str
+            "message: "
+            message))
+       ))
+   ))
+
+(defn chat-ws-fn
+  "Establish websocket connection with server"
+  []
+  (websocket
+    rurls/chat-url
+    {:onopen-fn establish-chat-connection-ws-fn
+     :onmessage-fn chat-ws-onmessage-fn
+     :onclose-fn websocket-default-close}))
 
--- src/cljs/common_client/chat/view.cljs
+++ src/cljs/common_client/chat/view.cljs
@@ -4,146 +4,23 @@
             [common-client.chat.controller :as cc]
             [language-lib.core :refer [get-label]]))
 
-(defn generate-chat-history
-  "Generate html of chat history"
-  [chat-history
-   logged-in-username]
-  (let [messages (atom [])]
-    (doseq [{username :username
-             text :text
-             sent-at :sent-at} chat-history]
-      (swap!
-        messages
-        conj
-        (div
-          (div
-            text
-            {:class (if (= username
-                           logged-in-username)
-                      "my-message"
-                      "contacts-message")})
-          {:class (if (= username
-                         logged-in-username)
-                    "my-message-container"
-                    "contacts-message-container")})
-       ))
-    (gen
-      @messages))
- )
-
-(def refresh-on
-     (atom false))
-
-(def is-first-time
-     (atom true))
-
-(defn load-chat-history
-  "Load history in view"
-  [logged-in-username
-   & [element
-      event
-      recur-me]]
-  (let [chat-history-el (md/query-selector-on-element
-                          ".chat"
-                          ".chat-history")
-        scroll-value (atom nil)]
-    (if chat-history-el
-      (do
-        (when-not (empty?
-                    (.-innerHTML
-                      chat-history-el))
-          (let [scroll-top (.-scrollTop
-                             chat-history-el)
-                scroll-top-max (.-scrollTopMax
-                                 chat-history-el)]
-            (when-not (= scroll-top
-                         scroll-top-max)
-              (reset!
-                scroll-value
-                (.-scrollTop
-                  chat-history-el))
-             ))
-         )
-        (let [chat-history (cc/get-chat-history
-                             logged-in-username)
-              generated-chat-history (generate-chat-history
-                                       chat-history
-                                       logged-in-username)
-              generated-chat-history-in-div (gen
-                                              (div
-                                                generated-chat-history))]
-          (when-not (= (.-innerHTML
-                         generated-chat-history-in-div)
-                       (let [chat-history (md/query-selector-on-element
-                                            ".chat"
-                                            ".chat-history")
-                             chat-history-inner-html (.-innerHTML
-                                                       chat-history)]
-                         chat-history-inner-html))
-            (md/remove-element-content
-              ".chat-history")
-            (md/append-element
-              ".chat-history"
-              generated-chat-history))
-         )
-        (let [chat-history-el (md/query-selector-on-element
-                                ".chat"
-                                ".chat-history")]
-          (if @scroll-value
-            (aset
-              chat-history-el
-              "scrollTop"
-              @scroll-value)
-            (aset
-              chat-history-el
-              "scrollTop"
-              (.-scrollTopMax
-                chat-history-el))
-           ))
-        (when-not @refresh-on
-          (reset!
-            refresh-on
-            true))
-        (when (and @refresh-on
-                   (or @is-first-time
-                       recur-me))
-          (js/setTimeout
-            #(load-chat-history
-               logged-in-username
-               nil
-               nil
-               true)
-            1000))
-        (when @is-first-time
-          (reset!
-            is-first-time
-            false))
-       )
-      (do
-        (reset!
-          refresh-on
-          false)
-        (reset!
-          is-first-time
-          true))
-     ))
- )
-
 (defn chat-pure-html
   "Construct html chat view and append it"
   [logged-in-username]
+  (reset!
+    cc/logged-in-username-a
+    logged-in-username)
   (md/remove-element-content
     ".content")
-  (let [users (cc/get-chat-users
-                logged-in-username)
-        options (atom
-                  [(option
-                     (get-label 33))])
+  (let [users (cc/get-chat-users)
         chat-form (gen
                     (div
                       [(div
                          [(select
-                            (do
+                            (let [options (atom
+                                            [(option
+                                               (get-label 33)
+                                               {:value "-1"})])]
                               (doseq [{username :username} users]
                                 (swap!
                                   options
@@ -154,23 +31,17 @@
                                )
                               @options)
                             {:id "chat-users"}
-                            {:onchange {:evt-fn load-chat-history
-                                        :evt-p logged-in-username}})
+                            {:onchange {:evt-fn cc/selected-contact-username}})
                           (input
                             ""
                             {:id "chat-refresh"
                              :class "btn"
                              :value (get-label 69)
                              :type "button"}
-                            {:onclick {:evt-fn load-chat-history
-                                       :evt-p logged-in-username}})]
+                            {:onclick {:evt-fn cc/get-chat-history}})]
                          {:class "chat-commands"})
                        (div
-                         (let [chat-history (cc/get-chat-history
-                                              logged-in-username)]
-                           (generate-chat-history
-                             chat-history
-                             logged-in-username))
+                         ""
                          {:class "chat-history"})
                        (div
                          [(input
@@ -178,23 +49,34 @@
                             {:id "chat-message-input"
                              :type "text"
                              :placeholder (get-label 66)}
-                            {:onkeyup {:evt-fn cc/send-chat-message-on-enter
-                                       :evt-p {:evt-fn load-chat-history
-                                               :evt-p logged-in-username}}
-                             })
+                            {:onkeyup {:evt-fn cc/send-chat-message-on-enter}})
                           (input
                             ""
                             {:class "btn"
                              :type "button"
                              :value (get-label 67)}
-                            {:onclick {:evt-fn cc/send-chat-message
-                                       :evt-p {:evt-fn load-chat-history
-                                               :evt-p logged-in-username}}
-                             })]
-                         {:class "chat-message"})]
+                            {:onclick {:evt-fn cc/send-chat-message-ws}})]
+                         {:class "chat-message"})
+                       #_(div
+                         [(input
+                            ""
+                            {:class "btn"
+                             :type "button"
+                             :value "Start streaming audio"}
+                            {:onclick {:evt-fn cc/start-streaming}})
+                          (input
+                            ""
+                            {:class "btn"
+                             :type "button"
+                             :value "Stop streaming audio"}
+                            {:onclick {:evt-fn cc/stop-streaming}})
+                          ]
+                         {:class "audio-message"})
+                       ]
                       {:class "chat"}))]
     (md/append-element
       ".content"
-      chat-form))
+      chat-form)
+    (cc/chat-ws-fn))
  )
 
