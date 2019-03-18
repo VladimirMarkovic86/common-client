e496156012a392b718b66db10e57aec2c5218ed9
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-18 16:26:53 +0100

    Task 63: Fixed optimization advanced compiling

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/common-client "0.3.17"
+(defproject org.clojars.vladimirmarkovic86/common-client "0.3.18"
   :description "Common client"
   :url "https://github.com/VladimirMarkovic86/common-client"
   :license {:name "Eclipse Public License"
@@ -8,12 +8,12 @@
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.8"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.3"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.8"]
-                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.21"]
+                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.22"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.6"]
                  [org.clojars.vladimirmarkovic86/client-test-lib "0.2.8"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.15"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.16"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.11"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.12"]
                  [org.clojars.vladimirmarkovic86/websocket-lib "0.1.1"]
                  ]
 
