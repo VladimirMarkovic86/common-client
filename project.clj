e0bcd0d6855a8a69dbb6791539646bf93aafcc60
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-28 15:55:26 +0200

    Task 63: Implemented tests for js-lib.core namespace.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/common-client "0.3.28"
+(defproject org.clojars.vladimirmarkovic86/common-client "0.3.29"
   :description "Common client"
   :url "https://github.com/VladimirMarkovic86/common-client"
   :license {:name "Eclipse Public License"
@@ -7,13 +7,13 @@
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.9"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.6"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.12"]
-                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.32"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.13"]
+                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.33"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.9"]
                  [org.clojars.vladimirmarkovic86/client-test-lib "0.2.11"]
                  [org.clojars.vladimirmarkovic86/language-lib "0.2.25"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.6"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.22"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.23"]
                  [org.clojars.vladimirmarkovic86/websocket-lib "0.1.1"]
                  ]
 
