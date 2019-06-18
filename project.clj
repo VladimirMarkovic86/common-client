1ba98490796e8ea48cf1996626e683ed51fe2b13
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-06-18 16:16:40 +0200

    Task 63: validator-lib.core called JSON object properties with .- (dot dash) operand, now is switched and is calling properties with aget clojure array function.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/common-client "0.3.37"
+(defproject org.clojars.vladimirmarkovic86/common-client "0.3.38"
   :description "Common client"
   :url "https://github.com/VladimirMarkovic86/common-client"
   :license {:name "Eclipse Public License"
@@ -8,12 +8,12 @@
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.11"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.6"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.15"]
-                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.40"]
+                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.41"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.9"]
                  [org.clojars.vladimirmarkovic86/client-test-lib "0.2.12"]
                  [org.clojars.vladimirmarkovic86/language-lib "0.2.30"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.8"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.29"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.30"]
                  [org.clojars.vladimirmarkovic86/websocket-lib "0.1.2"]
                  ]
 
