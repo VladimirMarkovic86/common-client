343a45d51ec1d64217c283c67f66bb33da6c8442
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:17:28 +0200

    Task 88: Excluded public folder from some more clojurescripts projects packing jar file.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/common-client "0.3.44"
+(defproject org.clojars.vladimirmarkovic86/common-client "0.3.45"
   :description "Common client"
   :url "https://github.com/VladimirMarkovic86/common-client"
   :license {:name "Eclipse Public License"
@@ -8,13 +8,13 @@
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.12"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.7"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.17"]
-                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.46"]
+                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.47"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
-                 [org.clojars.vladimirmarkovic86/client-test-lib "0.2.15"]
+                 [org.clojars.vladimirmarkovic86/client-test-lib "0.2.16"]
                  [org.clojars.vladimirmarkovic86/language-lib "0.2.34"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.35"]
-                 [org.clojars.vladimirmarkovic86/websocket-lib "0.1.3"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.36"]
+                 [org.clojars.vladimirmarkovic86/websocket-lib "0.1.4"]
                  ]
 
   :min-lein-version "2.0.0"
